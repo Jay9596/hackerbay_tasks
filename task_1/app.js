31dbb8ba15f2a1ae34829a5af2febce2bdaf7379
Jay Shah <jayshah.9596@gmail.com>
2018-08-20 22:05:32 +0530

    Task 1 complete

---
+++ task_1/app.js
@@ -0,0 +1,38 @@
+var express = require("express");
+
+var data = null; // Global variable stores the data from POST
+
+var app = express();
+// Middleware to parse request body
+var bodyParser = require("body-parser");
+app.use(bodyParser.json()); // support json objects
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.get("/", (req, res) => {
+  res.status(200).send({ status: "success" });
+  res.end();
+});
+
+app.post("/data", (req, res) => {
+  data = req.body.data;
+  res.status(200).send({ data });
+  res.end();
+});
+
+app.get("/data", (req, res) => {
+  if (data != undefined || data != null) {
+    res.status(200).send({ data });
+  } else {
+    res.sendStatus(404);    // Send 404, if data is not present
+  }
+  res.end();
+});
+
+const PORT = 8000;
+app.listen(PORT, err => {
+  if (err) {
+    throw err;
+  }
+});
+
+console.log("Server started on " + PORT);
