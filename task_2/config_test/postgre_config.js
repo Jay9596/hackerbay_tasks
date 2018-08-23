9f650865973c72b5e42a46828846e0785ce3923d
Jay Shah <jayshah.9596@gmail.com>
2018-08-24 01:02:13 +0530

    Sample config files

---
+++ task_2/config_test/postgre_config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  database: "Your_Database_Name",
+  user: "Username",
+  password: "Password",
+  host: "localhost", // localhost
+  port: 5432  // default port
+};
---
+++ task_2/config_test/secret.js
@@ -0,0 +1,4 @@
+module.exports = {
+    secretKey: 'Your_Secret_Key'
+    // and any other secret
+}
