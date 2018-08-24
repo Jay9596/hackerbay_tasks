3f529709cb270d228e0c379f3d7ec7181ab4337e
Jay Shah <jayshah.9596@gmail.com>
2018-08-24 12:32:40 +0530

    Address Issue :hammer:

--- task_1/app.js
+++ task_1/app.js
@@ -9,19 +9,19 @@ app.use(bodyParser.json()); // support json objects
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get("/", (req, res) => {
-  res.status(200).send({ status: "success" });
+  res.status(200).json({ status: "success" });
   res.end();
 });
 
 app.post("/data", (req, res) => {
   data = req.body.data;
-  res.status(200).send({ data });
+  res.status(200).json({ data });
   res.end();
 });
 
 app.get("/data", (req, res) => {
   if (data != undefined || data != null) {
-    res.status(200).send({ data });
+    res.status(200).json({ data });
   } else {
     res.sendStatus(404);    // Send 404, if data is not present
   }
@@ -33,6 +33,6 @@ app.listen(PORT, err => {
   if (err) {
     throw err;
   }
+  console.log("Server started on " + PORT);
 });
 
-console.log("Server started on " + PORT);
